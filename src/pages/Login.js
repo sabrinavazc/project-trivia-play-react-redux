78d38dfd226137e2c5593f19d4ecd8ae45649e3a
jp-almada <joaopedroaa200121@gmail.com>
2023-06-22 18:59:58 -0300

    feat: req5 - Created Header component and change variable names

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,49 +2,44 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { BsFillGearFill } from 'react-icons/bs';
 import { connect } from 'react-redux';
-import { getToken } from '../helpers/fetchTrivia';
 import { actionUser } from '../redux/actions';
+import { getToken } from '../helpers/fetchTrivia';
 
 class Login extends Component {
   state = {
-    gravatarEmail: '',
-    userName: '',
+    email: '',
+    name: '',
   };
 
   handleChange = ({ target: { name, value } }) => {
     this.setState({ [name]: value });
   };
 
-  saveGlobalState = () => {
-    const { gravatarEmail, userName } = this.state;
-    const { dispatch } = this.props;
-    dispatch(actionUser(gravatarEmail, userName));
-  };
-
-  saveTokenAndRedirect = async () => {
-    const { history } = this.props;
-    const token = await getToken();
-    localStorage.setItem('token', token.token);
-    this.saveGlobalState();
+  handleClick = async () => {
+    const { history, dispatch } = this.props;
+    const { email, name } = this.state;
+    const { token } = await getToken();
+    localStorage.setItem('token', token);
+    dispatch(actionUser({ email, name }));
     history.push('/game');
   };
 
   render() {
-    const { gravatarEmail, userName } = this.state;
+    const { email, name } = this.state;
     const { history } = this.props;
     return (
       <main>
         <form>
           <input
             type="email"
-            name="gravatarEmail"
+            name="email"
             placeholder="Qual é o e-mail do seu gravatar?"
             data-testid="input-gravatar-email"
             onChange={ this.handleChange }
           />
           <input
             type="text"
-            name="userName"
+            name="name"
             placeholder="Qual seu nome?"
             data-testid="input-player-name"
             onChange={ this.handleChange }
@@ -52,9 +47,9 @@ class Login extends Component {
           <button
             type="button"
             data-testid="btn-play"
-            disabled={ userName.length < 1
-              || !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(gravatarEmail) }
-            onClick={ this.saveTokenAndRedirect }
+            disabled={ name.length < 1
+              || !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email) }
+            onClick={ this.handleClick }
           >
             Jogar
           </button>
